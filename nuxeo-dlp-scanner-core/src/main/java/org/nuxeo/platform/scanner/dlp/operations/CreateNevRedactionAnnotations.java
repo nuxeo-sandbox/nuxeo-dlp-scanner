2bba1a90dc92cca249e7b2502e09a256652cce65
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-09-19 15:25:09 +0900

    Create Annotations

---
+++ nuxeo-dlp-scanner-core/src/main/java/org/nuxeo/platform/scanner/dlp/operations/CreateNevRedactionAnnotations.java
@@ -0,0 +1,127 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Damon Brown
+ */
+package org.nuxeo.platform.scanner.dlp.operations;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.comment.api.AnnotationImpl;
+import org.nuxeo.ecm.platform.comment.api.AnnotationService;
+import org.nuxeo.platform.scanner.dlp.DLPScanConstants;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Create NEV annotations based on the stored dlp info
+ *
+ * @since 10.10
+ */
+@Operation(id = CreateNevRedactionAnnotations.ID, category = "NEV",
+        label = "Create DLP NEV Redaction Annotations",
+        description = "Create NEV Redaction annotations based on the stored dlp info")
+public class CreateNevRedactionAnnotations {
+
+    public static final Logger log = LogManager.getLogger(CreateNevRedactionAnnotations.class);
+
+    public static final String ID = "NEV.CreateDlpAnnotations";
+
+    @Context
+    protected CoreSession session;
+
+    @Context
+    protected AnnotationService annotationService;
+
+    @Param(name = "xpath", required = false, values = {"file:content"})
+    protected String xpath = "file:content";
+
+    @OperationMethod(collector = DocumentModelCollector.class)
+    public DocumentModel run(DocumentModel doc) throws IOException {
+        if (!doc.hasFacet(DLPScanConstants.DLP_FACET)) {
+            log.warn("document {} {} doesn't have the DLP facet", doc.getId(), doc.getPathAsString());
+            return doc;
+        }
+
+        boolean hasSensitiveData = (boolean) doc.getPropertyValue(DLPScanConstants.DLP_SENSITIVE_DATA);
+
+        if (!hasSensitiveData) {
+            log.warn("document {} {} doesn't have any sensitive data", doc.getId(), doc.getPathAsString());
+            return doc;
+        }
+
+        List<Map<String, Serializable>> sensitiveData =
+                (List<Map<String, Serializable>>) doc.getPropertyValue(DLPScanConstants.DLP_FINDINGS);
+
+        if (sensitiveData == null || sensitiveData.isEmpty()) {
+            return doc;
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (Map<String, Serializable> item : sensitiveData) {
+            String locationJson = (String) item.get("locationJson");
+            if (StringUtils.isBlank(locationJson)) {
+                continue;
+            }
+            JsonNode json = mapper.readTree(locationJson);
+
+            boolean hasLocation = json.get("hasImageLocation").asBoolean();
+
+            if (!hasLocation) {
+                continue;
+            }
+
+            JsonNode locations = json.get("imageLocations");
+
+            locations.iterator().forEachRemaining(location -> {
+                AnnotationImpl annotation = new AnnotationImpl();
+                annotation.setParentId(doc.getId());
+                annotation.setXpath(xpath);
+                annotation = (AnnotationImpl) annotationService.createAnnotation(session, annotation);
+
+                annotation.setOrigin("ARender");
+
+                String annotationText = String.format("""
+                                <?xml version="1.0" encoding="UTF-8"?><ns0:xfdf xmlns:ns0="http://ns.adobe.com/xfdf/"><ns0:annots><ns0:redact color="#000000" flags="" name="%s" last-modifier="System" page="0" rect="%d,%d,%d,%d" title="%s" creationdate="D:20240919020457+00'00'" opacity="1.0" interior-color="#000000" overlay-text="(b)(2)"/></ns0:annots></ns0:xfdf>""",
+                        annotation.getId(),
+                        location.get("top").asInt(), location.get("left").asInt(),
+                        location.get("width").asInt(), location.get("height").asInt(),
+                        item.get("type"));
+
+                annotation.setEntityId(annotation.getId());
+                annotation.setEntity(annotationText);
+                annotationService.updateAnnotation(session, annotation.getId(), annotation);
+            });
+        }
+
+        return doc;
+    }
+
+}
---
+++ nuxeo-dlp-scanner-core/src/test/java/org/nuxeo/platform/scanner/dlp/TestNevRedationAnnotation.java
@@ -0,0 +1,88 @@
+package org.nuxeo.platform.scanner.dlp;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
+import org.nuxeo.ecm.core.test.annotations.Granularity;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.platform.comment.CommentFeature;
+import org.nuxeo.ecm.platform.comment.api.Annotation;
+import org.nuxeo.ecm.platform.comment.api.AnnotationService;
+import org.nuxeo.platform.scanner.dlp.operations.CreateNevRedactionAnnotations;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.TransactionalFeature;
+
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(FeaturesRunner.class)
+@Features({AutomationFeature.class, CommentFeature.class})
+@RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
+@Deploy("org.nuxeo.ecm.core.test")
+@Deploy("org.nuxeo.platform.scanner.dlp.core")
+@Deploy("org.nuxeo.platform.scanner.dlp.core:disable-listeners-contrib.xml")
+public class TestNevRedationAnnotation {
+
+    @Inject
+    protected CoreSession session;
+
+    @Inject
+    protected TransactionalFeature transactionalFeature;
+
+    @Inject
+    protected AutomationService automationService;
+
+    @Inject
+    protected AnnotationService annotationService;
+
+
+    @Test
+    public void testCreateAnnotation() throws Exception {
+
+        DocumentModel doc = session.createDocumentModel(
+                session.getRootDocument().getPathAsString(),"File","File");
+        doc.addFacet(DLPScanConstants.DLP_FACET);
+
+        Map<String, Serializable> sensitiveData = new HashMap<>();
+        sensitiveData.put("type","SSN");
+        sensitiveData.put("info","AAA-GG-SSS");
+        sensitiveData.put("score","LIKELY");
+        sensitiveData.put("locationJson", """
+                {"hasImageLocation":true,"imageLocations":[{"top":120,"left":220,"width":124,"height":27}],"hasCodepointRange":false,"hasByteRange":false,"firstImageLocation":{"top":120,"left":220,"width":124,"height":27}}
+                """);
+
+        doc.setPropertyValue("dlp:sensitiveData", true);
+        doc.setPropertyValue("dlp:scanStatus", "done");
+        doc.setPropertyValue("dlp:scanDate", "2024-09-19T00:39:17.688Z");
+        doc.setPropertyValue("dlp:findings", (Serializable) List.of(sensitiveData));
+
+        doc = session.createDocument(doc);
+
+        transactionalFeature.nextTransaction();
+
+        OperationContext ctx = new OperationContext(session);
+        ctx.setInput(doc);
+        doc = (DocumentModel) automationService.run(ctx, CreateNevRedactionAnnotations.ID);
+
+        transactionalFeature.nextTransaction();
+
+        List<Annotation> annotations = annotationService.getAnnotations(session,doc.getId(),"file:content");
+
+        Assert.assertEquals(1,annotations.size());
+
+    }
+
+
+
+}
